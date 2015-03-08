b76708e1100e153a6f7ad5d71cf8c356d4d2eda9
Gary Sone <gary.sone@bh.sone.net>
2015-03-08 18:34:02 +0000

    Create setup-brightpi.sh

---
+++ brightpi/setup-brightpi.sh
@@ -0,0 +1,8 @@
+!#/bin/bash -v
+
+# script to setup BrightPi - https://learn.adafruit.com/adafruits-raspberry-pi-lesson-4-gpio-setup/configuring-i2c
+
+# sudo apt-get install python-smbus
+# sudo apt-get install i2c-tools
+# sudo raspi-config
+# sudo i2cdetect -y 1
